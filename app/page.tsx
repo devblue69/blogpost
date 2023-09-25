b0083bba740237714181c6bd322df152890115f5
devblue69 <devblue69@gmail.com>
2023-09-25 14:42:36 +0530

    new changes made by me

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Welcome to blOgAPP&nbsp;
+          new changes made &nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
